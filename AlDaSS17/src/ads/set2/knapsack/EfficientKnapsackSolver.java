2fb9597f7f7eee04a0ca54d612d5929f45c97894
YaAlex <yaalex.devlab@gmail.com>
2017-05-17 20:29:50 +0200

    rucksack 1 done

---
+++ AlDaSS17/src/ads/set2/knapsack/EfficientKnapsackSolver.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package ads.set2.knapsack;
+
+/**
+ * @author Yannik Eikmeier
+ * @since 17.05.2017
+ *
+ */
+public class EfficientKnapsackSolver {
+	/**
+	 * Calculates the maximum profit for the knapsack problem using an improved
+	 * dynamic programming algorithm
+	 * 
+	 * @param items
+	 *            the items available to be packed into the knapsack.
+	 * @param capacity
+	 *            the maximum weight allowed for the knapsack.
+	 * @return the maximum profit possible for the given weight
+	 */
+	public static int pack(final Item[] items, final int capacity) {
+		// TODO Implement me!
+		return 0;
+	}
+}
--- AlDaSS17/src/ads/set2/knapsack/KnapsackSolver.java
+++ AlDaSS17/src/ads/set2/knapsack/KnapsackSolver.java
@@ -1,5 +1,8 @@
 package ads.set2.knapsack;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 /**
  * @author Yannik Eikmeier
  * @since 17.05.2017
@@ -7,6 +10,20 @@ package ads.set2.knapsack;
  */
 public class KnapsackSolver {
 
+	public static void main(String[] args) {
+		
+		Item i0 = new Item(1, 1);
+		Item i1 = new Item(1, 2);
+		Item i2 = new Item(2, 3);
+		Item i3 = new Item(3, 4);
+		Item i4 = new Item(1, 4);
+		Item i5 = new Item(2, 1);
+		
+		
+		Packing p = pack(new Item[]{i0,i1,i2,i3,i4,i5}, 6);
+	}
+	
+	
 	/**
 	 * Solves the knapsack problem using the dynamic programming algorithm
 	 * introduced in the lecture.
@@ -19,8 +36,60 @@ public class KnapsackSolver {
 	 *         into the knapsack as well as the profit yielded by doing so.
 	 */
 	public static Packing pack(final Item[] items, final int capacity) {
-	    // TODO Implement me!
-	    return null;
+
+		final int n = items.length;
+		final int infinit = capacity + 1;
+		int maxProfit = 0;
+		for (Item i : items)
+			maxProfit += i.getProfit();
+
+		int[][] weight = new int[n][maxProfit + 1];
+		// field[0][0] = 0;
+
+		for (int t = 1; t <= maxProfit; t++) {
+			if (items[0].getProfit() != t)
+				weight[0][t] = infinit;
+			else
+				weight[0][t] = items[0].getWeight();
+		}
+
+		for (int i = 1; i < weight.length; i++) {
+			for (int profit = 0; profit < weight[0].length; profit++) {
+
+				Item item = items[i];
+				int w = item.getWeight();
+				int p = item.getProfit();
+
+				if (profit < p || weight[i - 1][profit] < weight[i - 1][profit - p] + w)
+					// keine verbesserung
+					weight[i][profit] = weight[i - 1][profit];
+				else
+					// verbesserung durch neues item
+					weight[i][profit] = weight[i - 1][profit - p] + w;
+			}
+		}
+
+		Packing pack = new Packing();
+		Collection<Item> resItems = pack.getItems();
+		int profit;
+		for (profit = maxProfit; profit > 0; profit--) {
+			if (weight[n - 1][profit] <= capacity){
+				System.out.println(profit);
+				
+				break;
+			}
+		}
+		pack.setProfit(profit);
+		for (int i = n - 1; i >= 0; i--) {
+
+			if ((i > 0 && weight[i - 1][profit] > weight[i][profit]) || (i == 0 && profit != 0)) {
+				resItems.add(items[i]);
+				profit -= items[i].getProfit();
+			}
+
+		}
+
+		return pack;
 	}
-	
+
 }
