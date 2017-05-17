13fcaedcc6e9e405ffabbb7549b69fa59393eac5
YaAlex <yaalex.devlab@gmail.com>
2017-05-17 19:32:42 +0200

    init set2

---
+++ AlDaSS17/src/ads/set2/knapsack/KnapsackSolver.java
@@ -0,0 +1,26 @@
+package ads.set2.knapsack;
+
+/**
+ * @author Yannik Eikmeier
+ * @since 17.05.2017
+ *
+ */
+public class KnapsackSolver {
+
+	/**
+	 * Solves the knapsack problem using the dynamic programming algorithm
+	 * introduced in the lecture.
+	 * 
+	 * @param items
+	 *            the items available to be packed into the knapsack.
+	 * @param capacity
+	 *            the maximum weight allowed for the knapsack.
+	 * @return a packing with maximum profit which contains the items to pack
+	 *         into the knapsack as well as the profit yielded by doing so.
+	 */
+	public static Packing pack(final Item[] items, final int capacity) {
+	    // TODO Implement me!
+	    return null;
+	}
+	
+}
